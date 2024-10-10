757e2161be475fad323c67706ffa9ac2770c55ca
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2024-10-10 19:29:21 +0200

    Add loging

--- app/src/main/java/ch/heigvd/iict/daa/lab2/MainActivityEdit.kt
+++ app/src/main/java/ch/heigvd/iict/daa/lab2/MainActivityEdit.kt
@@ -2,13 +2,16 @@ package ch.heigvd.iict.daa.lab2
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivityEdit : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
         setContentView(R.layout.activity_main_edit)
         val textField = findViewById<EditText>(R.id.nameView)
         val button = findViewById<Button>(R.id.button)
@@ -21,7 +24,33 @@ class MainActivityEdit : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
     companion object {
+        const val TAG = "MainActivityEdit"
         const val ASK_FOR_NAME_RESULT_KEY = "NAME_KEY"
     }
 }
--- app/src/main/java/ch/heigvd/iict/daa/lab2/MainActivityWelcome.kt
+++ app/src/main/java/ch/heigvd/iict/daa/lab2/MainActivityWelcome.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContract
@@ -35,11 +36,40 @@ class MainActivityWelcome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
         setContentView(R.layout.activity_main_welcome)
         textView = findViewById(R.id.nameView)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
             getName.launch(null)
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
+    companion object {
+        const val TAG = "MainActivityWelcome"
     }
 }
