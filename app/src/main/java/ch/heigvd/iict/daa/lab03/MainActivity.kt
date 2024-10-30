3a27149899b1c78f58f12513fbb4afbaf7bc2885
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-10-30 15:03:37 +0100

    add datepickerdialog and ok behaviour

--- app/src/main/java/ch/heigvd/iict/daa/lab03/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/lab03/MainActivity.kt
@@ -1,5 +1,6 @@
 package ch.heigvd.iict.daa.lab03
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,8 +9,13 @@ import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import ch.heigvd.iict.daa.labo3.Student
+import ch.heigvd.iict.daa.labo3.Worker
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var calendar: Calendar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         employeeEditTexts.forEach { it.visibility = View.GONE }
         employeeSpinner.visibility = View.GONE
 
+        // Radio buttons
         val studentRadioButton = findViewById<Button>(R.id.main_base_occupation_student)
         studentRadioButton.setOnClickListener {
             studentTextViews.forEach { it.visibility = View.VISIBLE }
@@ -62,6 +69,9 @@ class MainActivity : AppCompatActivity() {
             employeeSpinner.visibility = View.VISIBLE
         }
 
+        //TODO spinners setup
+
+        // Cancel button
         val cancelButton = findViewById<Button>(R.id.btn_cancel)
         cancelButton.setOnClickListener {
             //TODO clear all other fields
@@ -69,14 +79,54 @@ class MainActivity : AppCompatActivity() {
             employeeEditTexts.forEach { it.text.clear() }
         }
 
+        // OK button
         val okButton = findViewById<Button>(R.id.btn_ok)
         okButton.setOnClickListener {
-            //TODO add the new person to the list
+            //TODO check that fields are not null ?
+            val Person = if (studentRadioButton.isSelected) {
+                Student(
+                    findViewById<EditText>(R.id.main_base_name).text.toString(),
+                    findViewById<EditText>(R.id.main_base_firstname).text.toString(),
+                    calendar,
+                    findViewById<EditText>(R.id.nationality).toString(),
+                    findViewById<EditText>(R.id.main_specific_school).text.toString(),
+                    findViewById<EditText>(R.id.main_specific_graduationyear).text.toString().toInt(),
+                    findViewById<EditText>(R.id.main_complementary_email).text.toString(),
+                    findViewById<EditText>(R.id.main_complementary_remarks).text.toString()
+                )
+            } else {
+                Worker(
+                    findViewById<EditText>(R.id.main_base_name).text.toString(),
+                    findViewById<EditText>(R.id.main_base_firstname).text.toString(),
+                    calendar,
+                    findViewById<EditText>(R.id.nationality).toString(),
+                    findViewById<EditText>(R.id.main_specific_company).text.toString(),
+                    findViewById<Spinner>(R.id.sector).selectedItem.toString(),
+                    findViewById<EditText>(R.id.main_specific_experience).text.toString().toInt(),
+                    findViewById<EditText>(R.id.main_complementary_email).text.toString(),
+                    findViewById<EditText>(R.id.main_complementary_remarks).text.toString()
+                )
+            }
+
+            println("created : $Person")
         }
 
+        // Calendar and DatePickerDialog
+        calendar = Calendar.getInstance()
         val cakeButton = findViewById<ImageButton>(R.id.cake)
         cakeButton.setOnClickListener {
-            //TODO call datepicker API
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog =
+                DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                    calendar.set(selectedYear, selectedMonth, selectedDay)
+                    val birthdateTextView = findViewById<TextView>(R.id.main_base_birthdate)
+                    birthdateTextView.text = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                }, year, month, day)
+
+            datePickerDialog.show()
         }
     }
 }
