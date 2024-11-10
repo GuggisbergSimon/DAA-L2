3f0bc8df319babe8d82949d1bf105fcc174eb32f
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-11-10 16:00:48 +0100

    implement recyclerview for notes
    details to be clarified during labo periods

--- app/src/main/java/ch/heigvd/iict/daa/labo4/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/labo4/MainActivity.kt
@@ -5,6 +5,9 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ch.heigvd.iict.daa.labo4.models.Note
 
 class MainActivity : AppCompatActivity() {
     private val notesViewModel: NotesViewModel by viewModels()
@@ -41,10 +44,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val adapter = MyRecyclerAdapter()
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this)
+        adapter.items = listOf(
+            Note.generateRandomNote(),
+            Note.generateRandomNote(),
+            Note.generateRandomNote(),
+            Note.generateRandomNote(),
+            Note.generateRandomNote(),
+            Note.generateRandomNote(),
+            Note.generateRandomNote(),
+        )
+
         /* TODO phase 2
         - La MainActivity et ses Layouts (smartphone et tablette) ;
-        - Le Menu associé à l’Activité (tri par date de création, tri par date de réalisation prévue, création
-          d’une Note (aléatoire) et suppression de toutes les Notes ;
         - Le Fragment et son Layout affichant la liste des Notes ;
         - Le Fragment et son Layout qui contiendra le compteur et deux boutons de contrôles (sur
           tablette uniquement, remplaçant les entrées du Menu permettant la création aléatoire d’une
@@ -52,3 +67,11 @@ class MainActivity : AppCompatActivity() {
          */
     }
 }
+
+abstract class Animal(var name : String, val id: Long = ++Animal.id) {
+    companion object {
+        private var id = 0L
+    }
+}
+class Mammal(name : String) : Animal(name)
+class Aves(name : String) : Animal(name)
---
+++ app/src/main/java/ch/heigvd/iict/daa/labo4/RecyclerAdapter.kt
@@ -0,0 +1,77 @@
+package ch.heigvd.iict.daa.labo4
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import ch.heigvd.iict.daa.labo4.models.State
+import ch.heigvd.iict.daa.labo4.models.Note
+import ch.heigvd.iict.daa.labo4.models.Type
+import java.util.Locale
+
+class MyRecyclerAdapter(_items: List<Note> = listOf()) :
+    RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder>() {
+    var items = listOf<Note>()
+        set(value) {
+            field = value
+            notifyDataSetChanged() //à éviter, on préférera utiliser DiffUtil pour propager les modifications effectives uniquement (cf. slides suivants)
+        }
+
+    init {
+        items = _items
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int): Int {
+        //TODO
+        return 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val icon = view.findViewById<ImageView>(R.id.list_item_icon)
+        private val title = view.findViewById<TextView>(R.id.list_item_title)
+        private val text = view.findViewById<TextView>(R.id.list_item_text)
+        private val progressIcon = view.findViewById<ImageView>(R.id.list_item_progress_icon)
+        private val progressText = view.findViewById<TextView>(R.id.list_item_progress_text)
+        fun bind(note: Note) {
+            icon?.setImageResource(
+                when (note.type) {
+                    Type.SHOPPING -> R.drawable.shopping
+                    Type.TODO -> R.drawable.todo
+                    Type.WORK -> R.drawable.work
+                    Type.FAMILY -> R.drawable.family
+                    Type.NONE -> TODO()
+                }
+            )
+
+            if (note.state == State.DONE) {
+                icon?.setColorFilter(ContextCompat.getColor(itemView.context, R.color.green))
+                progressIcon?.visibility = View.INVISIBLE
+                progressText?.visibility = View.INVISIBLE
+            } else {
+                icon?.clearColorFilter()
+                progressIcon?.visibility = View.VISIBLE
+                progressText?.visibility = View.VISIBLE
+                val dateFormat = java.text.DateFormat.getDateInstance(java.text.DateFormat.LONG, Locale.getDefault())
+                progressText?.text = dateFormat.format(note.creationDate.time)
+            }
+
+            title?.text = note.title
+            text?.text = note.text
+        }
+    }
+}
