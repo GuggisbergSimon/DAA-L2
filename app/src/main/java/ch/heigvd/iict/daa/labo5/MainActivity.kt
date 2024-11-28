98d417b3a2e7544a6a3ee5f8c014361215adcd52
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-11-28 11:11:03 +0100

    basic mainactivity and main menu

--- app/src/main/java/ch/heigvd/iict/daa/labo5/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/labo5/MainActivity.kt
@@ -1,12 +1,28 @@
 package ch.heigvd.iict.daa.labo5
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.main_menu_refresh -> {
+                //TODO refresh
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
